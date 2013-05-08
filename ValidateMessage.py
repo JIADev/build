addc0d31f436fb3081812e90a383a410e84d9446
Bryn Keller <bryn.keller@jenkon.com>
2013-05-08 13:26:38 -0700

    Added hook that prompts to build & run tests before commit @ignore

--- ValidateMessage.py
+++ ValidateMessage.py
@@ -22,6 +22,7 @@
 
 import re
 import subprocess
+import os
 
 from mercurial import commands, extensions, util
 import mercurial
@@ -57,15 +58,38 @@ def validate_not_direct_version_commit(ui, repo, *pats, **opts):
     if direct:
         raise util.Abort("Cannot commit directly to a numbered version, make changes in another branch and merge instead")
 
+def validate_built_and_tested(ui, repo, *pats, **opts):
+    rev = repo[None]
+    needs_test = not opts['close_branch'] and not rev.tags()
+    if needs_test:
+        if ui.prompt("Would you like to build j6 and run the unit tests before committing? [Yn]", default = 'y') in ('y', 'Y'):
+            tools = os.getcwd() + r"\tools.sln"
+            is_7_6_or_greater = os.path.exists(tools)
+            try:
+                if is_7_6_or_greater:
+                    subprocess.check_call(["msbuild", "/t:Build", "j6.proj"])
+                else:
+                    subprocess.check_call([r"j6\core\boot\feature", "build"])                    
+            except:
+                raise util.Abort("Build failed")
+            try:
+                if is_7_6_or_greater:
+                    test = subprocess.check_call(["msbuild", "/t:UnitTest", "j6.proj"])
+                else:
+                    test = subprocess.check_call([r"j6\core\boot\feature", "test"])
+            except:
+                raise util.Abort("Tests failed")
+            return opts['message'] + "\r\n** j6 unit tests pass **"
 
 # Validate Mercurial commit message.
 def validate_message(original_commit, ui, repo, *pats, **opts):
     if opts['subrepos']:
         validate_same_branch(ui, repo, *pats, **opts)
     validate_not_direct_version_commit(ui, repo, *pats, **opts)
+    updated_message = validate_built_and_tested(ui, repo, *pats, **opts)
     # Retrieve commit message.
-    message = opts['message']
-
+    message = updated_message or opts['message']
+    opts['message'] = message
     # Are we not doing a merge, tag or ignore?
     if not re.search('@[Mm]erge|@[Tt]ag|@[Ii]gnore', message):
 
