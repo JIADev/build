0cf1aa15e6b70a840e09e57ca7950b7d9382b4c6
carl.staub <carl.staub@localhost>
2014-02-25 14:10:43 -0800

    Attempt at revision history.

---
+++ Code/GetLastModifiedFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ExtractChangesets
+{
+	class Program
+	{
+		private static int Main(string[] args)
+		{
+			
+			if (args.Length < 0)
+			{
+				Console.WriteLine("Usage GetLastModifiedFile.exe <directory> [<searchPattern>]");
+				return 1;
+			}
+
+			var directory = new DirectoryInfo(args[0]);
+			
+			Console.WriteLine(
+				(args.Length > 1 ? directory.GetFiles(args[1]) : directory.GetFiles())
+				.OrderByDescending(f => f.LastWriteTimeUtc)
+				.Select(f => f.FullName)
+				.FirstOrDefault());
+
+			return 0;
+		}
+
+	}
+}
