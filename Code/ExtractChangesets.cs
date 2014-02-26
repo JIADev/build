90259e1d4df2af787be9506fab52eb88f06318c3
carl.staub <carl.staub@localhost>
2014-02-25 20:10:06 -0800

    Separating publish task from merge task

--- Code/ExtractChangesets.cs
+++ Code/ExtractChangesets.cs
@@ -22,7 +22,7 @@ namespace ExtractChangesets
 				var hgExe = "hg";
 				if (args.Length < 0)
 				{
-					Console.WriteLine("Usage ExtractChangesets.exe <mergedRevisions.txt.file> <outputFile.xml> <ccnetLogFile.xml> [<path.to.hg.exe>] (defaults to {0})",
+					Console.WriteLine("Usage ExtractChangesets.exe <mergedRevisions.txt.file> <outputFile.xml> [<path.to.hg.exe>] (defaults to {0})",
 						hgExe);
 					return 1;
 				}
@@ -34,8 +34,7 @@ namespace ExtractChangesets
 					return 2;
 				}
 				string outputFile = null;
-				string buildLogFile = null;
-
+				
 				if (args.Length > 1)
 				{
 					outputFile = args[1];
@@ -43,18 +42,12 @@ namespace ExtractChangesets
 
 				if (args.Length > 2)
 				{
-					buildLogFile = args[2];
-				}
-
-				if (args.Length > 3)
-				{
-					hgExe = args[3];
+					hgExe = args[2];
 				}
 
 				var changesets = GetChangesets(mergedRevisionFile);
-				var xml = GenerateXml(hgExe, outputFile, changesets);
-				if (buildLogFile != null)
-					MergeXml(buildLogFile, xml);
+				GenerateXml(hgExe, outputFile, changesets);
+				
 			}
 			catch (Exception ex)
 			{
@@ -63,112 +56,8 @@ namespace ExtractChangesets
 			}
 			return 0;
 		}
-
-		private static void MergeXml(string buildLogFile, XDocument revisionLog)
-		{
-			XDocument logDoc;
-			using (var logFile = new FileStream(buildLogFile, FileMode.Open, FileAccess.Read, FileShare.Read))
-			{
-				logDoc = XDocument.Load(logFile);
-			}
-			var mods = logDoc.XPathSelectElements("/cruisecontrol/modifications/modification").ToArray();
-			mods.Remove();
-
-			var modificationsElement = logDoc.XPathSelectElement("/cruisecontrol/modifications");
-			
-			var newRevisions = revisionLog.XPathSelectElements("/log/logentry");
-			
-			foreach (var logEntry in newRevisions.Select(CreateLogEntry))
-			{
-				modificationsElement.Add(logEntry.ToXElement());
-			}
-			
-			logDoc.Save(buildLogFile);
-		}
-
-		private class LogEntry
-		{
-			// ReSharper disable UnusedAutoPropertyAccessor.Local
-			// ReSharper disable MemberCanBePrivate.Local
-			public int? Revision { get; set; }
-			public string Node { get; set; }
-			public string ParentRevision { get; set; }
-			public string ParentNode { get; set; }
-			public string Branch { get; set; }
-			public string Author { get; set; }
-			public string Email { get; set; }
-			public DateTime? Date { get; set; }
-			public string Msg { get; set; }
-			// ReSharper restore MemberCanBePrivate.Local
-			// ReSharper restore UnusedAutoPropertyAccessor.Local
-
-			public XElement ToXElement()
-			{
-				var returnValue = new XElement("modification");
-				
-				returnValue.Add(new XAttribute("type", "Changeset"));
-
-				if(Date.HasValue)
-					returnValue.Add(CreateElement("date", Date.Value.ToString("yyyy-MM-dd HH:mm:ss")));
-				
-				if(!string.IsNullOrWhiteSpace(Author))
-					returnValue.Add(CreateElement("user", Author));
-				
-				if(!string.IsNullOrWhiteSpace(Msg))
-					returnValue.Add(CreateElement("comment", Msg));
-				
-				if (!string.IsNullOrWhiteSpace(Node))
-				{
-					returnValue.Add(Node.Length > 12
-						? CreateElement("changeNumber", Node.Substring(0, 12))
-						: CreateElement("changeNumber", Node));
-				}
-
-				if(!string.IsNullOrWhiteSpace(Node))
-					returnValue.Add(CreateElement("version", Node));
-				
-				if(!string.IsNullOrWhiteSpace(Email))
-					returnValue.Add(CreateElement("email", Email));
-				
-				return returnValue;
-			}
-
-			private XElement CreateElement(string elementName, string innerText)
-			{
-				var newElement = new XElement(elementName);
-				newElement.SetValue(innerText);
-				return newElement;
-			}
-		}
-
-		private static LogEntry CreateLogEntry(XElement logEntryElement)
-		{
-			var author = logEntryElement.XPathSelectElement("author");
-			var email = author == null ? null : author.Attribute("email");
-			var parent = logEntryElement.XPathSelectElement("parent");
-			var parentRevision = parent == null ? null : parent.Attribute("revision");
-			var parentNode = parent == null ? null : parent.Attribute("node");
-			var revision = logEntryElement.Attribute("revision");
-			var node = logEntryElement.Attribute("node");
-			var branch = logEntryElement.XPathSelectElement("branch");
-			var date = logEntryElement.XPathSelectElement("date");
-
-			var returnValue = new LogEntry
-			{
-				Revision = revision == null ? null : (int?)int.Parse(revision.Value),
-				Node = node == null ? null : node.Value,
-				Branch = branch == null ? null : branch.Value,
-				ParentRevision = parentRevision == null ? null : parentRevision.Value,
-				ParentNode = parentNode == null ? null : parentNode.Value,
-				Author = author == null ? null : author.Value,
-				Email = email == null ? null : email.Value,
-				Date = date == null ? null : (DateTime?)DateTime.Parse(date.Value),
-				Msg = logEntryElement.XPathSelectElement("msg").Value
-			};
-			return returnValue;
-		}
-
-		private static XDocument GenerateXml(string hgExe, string outputFile, IEnumerable<string> changesets)
+		
+		private static void GenerateXml(string hgExe, string outputFile, IEnumerable<string> changesets)
 		{
 			var args = CreateArgs(changesets);
 
@@ -182,7 +71,6 @@ namespace ExtractChangesets
 				entry.Remove();
 			}
 			xDoc.Save(outputFile);
-			return xDoc;
 		}
 
 		private static string RunProcess(string process,
