5591b5e1a094eaf0d3526d3315cc4f87a7ed0a4c
carl.staub <carl.staub@localhost>
2014-03-27 09:46:30 -0700

    Modifying track 3 build to be a tagged build

---
+++ createtracks2094.ps1
@@ -0,0 +1,7 @@
+$customernumber = '/p:CustomerNumber=2094;'
+$branches = 'Branches="REQ038;REQ145";'
+$buildTag = 'BuildTag=TRK3;'
+$baseTag = 'BaseTag=7.6.0'
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+& $msbuild /t:CreateBuild $customernumber$branches$buildTag$baseTag $scriptPath\buildtools.proj
