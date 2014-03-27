75e7f7e274daf4f952f9f5426eff5f096a0771e0
carl.staub <carl.staub@localhost>
2014-03-27 10:37:41 -0700

    Setting up the other track builds

--- createtracks2094.ps1
+++ createtracks2094.ps1
@@ -5,3 +5,20 @@ $baseTag = 'BaseTag=7.6.0'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 & $msbuild /t:CreateBuild $customernumber$branches$buildTag$baseTag $scriptPath\buildtools.proj
+
+$customernumber = '/p:CustomerNumber=2094;'
+$branches = 'Branches="REQ002";'
+$buildTag = 'BuildTag=TRK4;'
+$baseTag = 'BaseTag=7.6.0'
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+& $msbuild /t:CreateBuild $customernumber$branches$buildTag$baseTag $scriptPath\buildtools.proj
+
+$customernumber = '/p:CustomerNumber=2094;'
+$branches = 'Branches="REQ044;REQ049";'
+$buildTag = 'BuildTag=TRK5;'
+$baseTag = 'BaseTag=7.6.0'
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+& $msbuild /t:CreateBuild $customernumber$branches$buildTag$baseTag $scriptPath\buildtools.proj
+
