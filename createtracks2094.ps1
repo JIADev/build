f1b8af0a492c154017b8af48de307ecfa22f3faa
carl.staub <carl.staub@localhost>
2014-03-27 10:47:56 -0700

    slightly modifying the track build creation script

--- createtracks2094.ps1
+++ createtracks2094.ps1
@@ -1,4 +1,4 @@
-$customernumber = '/p:CustomerNumber=2094;'
+$customernumber = '/p:Interactive=false;CustomerNumber=2094;'
 $branches = 'Branches="REQ038;REQ145";'
 $buildTag = 'BuildTag=TRK3;'
 $baseTag = 'BaseTag=7.6.0'
@@ -6,7 +6,7 @@ $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 & $msbuild /t:CreateBuild $customernumber$branches$buildTag$baseTag $scriptPath\buildtools.proj
 
-$customernumber = '/p:CustomerNumber=2094;'
+$customernumber = '/p:Interactive=false;CustomerNumber=2094;'
 $branches = 'Branches="REQ177;REQ002;REQ060";'
 $buildTag = 'BuildTag=TRK4;'
 $baseTag = 'BaseTag=7.6.0'
