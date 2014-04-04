a99cd8ca5cb177329aca65ac241275bd4429ed60
tim.shattuck <tim.shattuck@localhost>
2014-04-04 09:39:33 -0700

    Createtracks test

--- createtracks2094.ps1
+++ createtracks2094.ps1
@@ -1,5 +1,5 @@
 $customernumber = '/p:Interactive=false;CustomerNumber=2094;'
-$branches = 'Branches="TRK3;REQ038;REQ145";'
+$branches = 'Branches="TRK3;REQ038;REQ145;REQ064;REQ147";'
 $buildTag = 'BuildTag=TRK3;'
 $baseTag = 'BaseTag=7.6.0'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
