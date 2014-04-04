7896731bf1acd863c290f9925b1b42a0926aa326
tim.shattuck <tim.shattuck@localhost>
2014-04-04 11:18:20 -0700

    Added Track1

---
+++ forwardmerge2094.ps1
@@ -0,0 +1,3 @@
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "c:\windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+& $msbuild /t:ForwardMerge_2094 $scriptPath\buildtools.proj
