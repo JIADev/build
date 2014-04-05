178378bf8a8e2fd9b7231ee8be74e1ff0cba23a7
carl.staub <carl.staub@localhost>
2014-04-05 02:49:47 -0700

    I wonder what *this* button does...

--- createtrack2.ps1
+++ createtrack2.ps1
@@ -1,7 +1,14 @@
-$customernumber = '/p:Interactive=false;CustomerNumber=2094;'
+$interactive = '/p:Interactive=true;'
+
+$args | foreach { if($_ -eq '--non-interactive'){
+      	$interactive = '/p:Interactive=false;'
+      }
+}
+
+$customernumber = 'CustomerNumber=2094;'
 $branches = 'Branches="TRK2;REQxxx;";'
 $buildTag = 'BuildTag=TRK2;'
 $baseTag = 'BaseTag=7.6.0'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
-& $msbuild /t:CreateBuild $customernumber$branches$buildTag$baseTag $scriptPath\buildtools.proj
+& $msbuild /t:CreateBuild $interactive$customernumber$branches$buildTag$baseTag $scriptPath\buildtools.proj
