db5a9be5d3e6c90d63135957ca5acb52c1fd8993
carl.staub <carl.staub@localhost>
2014-04-07 10:41:45 -0700

    Adding track 2 to build1.

--- createtracks2094.ps1
+++ createtracks2094.ps1
@@ -1,6 +1,6 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 & $scriptPath\createtrack1.ps1 --non-interactive
-
+& $scriptPath\createtrack2.ps1 --non-interactive
 & $scriptPath\createtrack3.ps1 --non-interactive
 & $scriptPath\createtrack4.ps1 --non-interactive
 & $scriptPath\createtrack5.ps1 --non-interactive
