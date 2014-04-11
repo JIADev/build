163c8a32201ce3f5cf08559f2ca527a3259e9687
carl.staub <carl.staub@localhost>
2014-04-11 11:01:53 -0700

    Adding REQ 16 to Track 4

--- createtrack4.ps1
+++ createtrack4.ps1
@@ -6,7 +6,7 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2094;'
-$branches = 'Branches="TRK4;REQ060;REQ177;REQ002";'
+$branches = 'Branches="TRK4;REQ060;REQ177;REQ002;REQ016";'
 $buildTag = 'BuildTag=TRK4;'
 $baseTag = 'BaseTag=7.6.0'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
