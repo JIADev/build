456b06fe4c0ae72603e85d2a5fed0030fe81bd90
carl.staub <carl.staub@localhost>
2014-04-26 00:51:50 -0700

    Updating create track scripts

--- createtrack3.ps1
+++ createtrack3.ps1
@@ -6,9 +6,12 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2094;'
-$branches = 'Branches="REQ038;REQ147;REQ064;REQ145;REQ011";'
+$branches = 'Branches="REQ038;REQ147;REQ064;REQ145;REQ011;REQ180"'
 $buildTag = 'BuildTag=TRK3;'
-$baseTag = 'BaseTag=7.6.0'
+$baseTag = 'BaseTag=7.6.0;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
-& $msbuild /t:CreateBuild $interactive$customernumber$branches$buildTag$baseTag $scriptPath\buildtools.proj
+& msbuild /t:UpdateReqs $interactive$customernumber$branches $scriptPath\buildtools.proj
+& msbuild /t:DependencyMerge_2094 /p:SourceRepo=c:\dev\repos\buildRepo $scriptPath\buildtools.proj
+& msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
+
