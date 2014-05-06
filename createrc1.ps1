2d2b2d8f026c91167ad687841bfc84302b508d1b
Greg Kim <gregory.kim@jenkon.com>
2014-05-06 15:27:56 -0700

    Bug #47122: Restore validation to original behavior.

---
+++ createrc1.ps1
@@ -0,0 +1,16 @@
+$interactive = '/p:Interactive=true;'
+
+$args | foreach { if($_ -eq '--non-interactive'){
+      	$interactive = '/p:Interactive=false;'
+      }
+}
+
+$customernumber = 'CustomerNumber=2094;'
+$branches = 'Branches="REQ044;REQ038;REQ146;REQ147;REQ064;REQ145;REQ132;REQ129;REQ126;REQ033;REQ036;REQ035;REQ002;REQ060;REQ177;REQ016"'
+$buildTag = 'BuildTag=RC1;'
+$baseTag = 'BaseTag=7.6.0;'
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+& msbuild /t:UpdateReqs $interactive$customernumber$branches $scriptPath\buildtools.proj
+& msbuild /t:DependencyMerge_2094 /p:SourceRepo=c:\dev\repos\buildRepo $scriptPath\buildtools.proj
+& msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
---
+++ createtrack1.ps1
@@ -0,0 +1,16 @@
+$interactive = '/p:Interactive=true;'
+
+$args | foreach { if($_ -eq '--non-interactive'){
+      	$interactive = '/p:Interactive=false;'
+      }
+}
+
+$customernumber = 'CustomerNumber=2094;'
+$branches = 'Branches="REQ033;REQ036;REQ035"'
+$buildTag = 'BuildTag=TRK1;'
+$baseTag = 'BaseTag=7.6.0;'
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+& msbuild /t:UpdateReqs $interactive$customernumber$branches $scriptPath\buildtools.proj
+& msbuild /t:DependencyMerge_2094 /p:SourceRepo=c:\dev\repos\buildRepo $scriptPath\buildtools.proj
+& msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
