dc00d6de41a5bcc7ba979fefbde225d29d3707ae
carl.staub <carl.staub@localhost>
2014-05-08 10:30:00 -0700

    Adding REQ 127 to track 2.

--- createtrack2.ps1
+++ createtrack2.ps1
@@ -6,7 +6,7 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2094;'
-$branches = 'Branches="REQ129;REQ132;REQ126;"'
+$branches = 'Branches="REQ129;REQ132;REQ127;REQ126;"'
 $buildTag = 'BuildTag=TRK2;'
 $baseTag = 'BaseTag=7.6.0;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
