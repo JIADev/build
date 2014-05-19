7683bc3986644379b66122dc158b1a9ebcdf9fc7
carl.staub <carl.staub@localhost>
2014-05-19 13:42:06 -0700

    Adding REQ 17 to track 1 build. @ignore

--- createtrack1.ps1
+++ createtrack1.ps1
@@ -6,7 +6,7 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2094;'
-$branches = 'Branches="REQ033;REQ036;REQ035;REQ039;REQ029;REQ031"'
+$branches = 'Branches="REQ033;REQ036;REQ035;REQ039;REQ029;REQ031;REQ017"'
 $buildTag = 'BuildTag=TRK1;'
 $baseTag = 'BaseTag=7.6.0;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
