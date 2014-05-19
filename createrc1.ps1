72186ec9b26923a20c04dd181b5a57d9ee682e04
carl.staub <carl.staub@localhost>
2014-05-19 13:50:28 -0700

    Changing build order due to new dependencies. @ignore

--- createrc1.ps1
+++ createrc1.ps1
@@ -6,7 +6,7 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2094;'
-$branches = 'Branches="REQ044;REQ038;REQ146;REQ147;REQ064;REQ145;REQ132;REQ129;REQ126;REQ033;REQ036;REQ035;REQ002;REQ060;REQ177;REQ016"'
+$branches = 'Branches="REQ044;REQ038;REQ146;REQ147;REQ064;REQ145;REQ132;REQ129;REQ126;REQ033;REQ060;REQ177;REQ036;REQ035;REQ002;REQ016"'
 $buildTag = 'BuildTag=RC1;'
 $baseTag = 'BaseTag=7.6.0;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
