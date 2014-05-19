242f876e9667259fe913285a09bdafa5ae56ba2c
carl.staub <carl.staub@localhost>
2014-05-19 13:56:57 -0700

    Updating update list @ignore

--- updatereqs2094.ps1
+++ updatereqs2094.ps1
@@ -1,6 +1,6 @@
 $interactive = '/p:Interactive=false;'
 $customernumber = 'CustomerNumber=2094;'
-$branches = 'Branches="REQ033;REQ036;REQ035;REQ039;REQ029;REQ031;REQ129;REQ132;REQ127;REQ126;REQ038;REQ146;REQ147;REQ064;REQ145;REQ011;REQ180;REQ016;REQ060;REQ177;REQ002;REQ006;REQ005;REQ081;REQ032;REQ049;REQ044"'
+$branches = 'Branches="REQ002;REQ005;REQ006;REQ011;REQ013;REQ016;REQ017;REQ029;REQ031;REQ032;REQ033;REQ035;REQ036;REQ038;REQ039;REQ044;REQ049;REQ060;REQ064;REQ081;REQ116;REQ126;REQ127;REQ129;REQ132;REQ141;REQ145;REQ146;REQ147;REQ177;REQ180;REQ193"'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "c:\windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 & $msbuild /t:UpdateReqs $interactive$customernumber$branches $scriptPath\buildtools.proj
