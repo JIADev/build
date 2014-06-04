4ba927959364ad49cd56e98f89c8c0049c2d08f4
carl.staub <carl.staub@localhost>
2014-06-04 03:05:11 -0700

    Configuring for 2095.

--- forwardmergeActive.ps1
+++ forwardmergeActive.ps1
@@ -1,4 +1,4 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "c:\windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 
-& $msbuild /t:ForwardMerge_2094 $scriptPath\buildtools.proj
+& $msbuild /t:ForwardMerge_Active $scriptPath\buildtools.proj
