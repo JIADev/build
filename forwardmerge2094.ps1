8dce989951f6932ce4f4fa020da606f918a06077
carl.staub <carl.staub@localhost>
2014-06-04 01:58:33 -0700

    Closing Release 1 REQ branches. @ignore

--- forwardmerge2094.ps1
+++ forwardmerge2094.ps1
@@ -1,5 +1,4 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "c:\windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 
-& $scriptPath/updatereqs2094.ps1
 & $msbuild /t:ForwardMerge_2094 $scriptPath\buildtools.proj
