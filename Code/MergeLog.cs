def953f855e4e20df7dc3fdc8a59f2241129e770
tim.shattuck <tim.shattuck@localhost>
2014-06-19 10:26:30 -0700

    @ updated CUST2094-RM47241

---
+++ Code/MergeLog.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace j6.BuildTools
+{
+	class Program
+	{
+		private static int Main(string[] args)
+		{
+			try
+			{
+
+				var inputFile = args[0];
+				var buildLogFile = args[1];
+
+				var xml = ReadXml(inputFile);
+				MergeXml(buildLogFile, xml);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				return 1;
+			}
+			return 0;
+		}
+
+		private static XDocument ReadXml(string inputFile)
+		{
+			XDocument returnValue;
+			using (var input = new FileStream(inputFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+			{
+				returnValue = XDocument.Load(input);
+			}
+			return returnValue;
+		}
+
+		private static void MergeXml(string buildLogFile, XDocument revisionLog)
+		{
+			XDocument logDoc;
+			using (var logFile = new FileStream(buildLogFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+			{
+				logDoc = XDocument.Load(logFile);
+			}
+			var mods = logDoc.XPathSelectElements("/cruisecontrol/modifications/modification").ToArray();
+			mods.Remove();
+
+			var modificationsElement = logDoc.XPathSelectElement("/cruisecontrol/modifications");
+
+			var newRevisions = revisionLog.XPathSelectElements("/log/logentry");
+
+			foreach (var logEntry in newRevisions.Select(LogEntry.CreateNew).Where(r => r.FilesModified != null && r.FilesModified.Any()))
+			{
+				modificationsElement.Add(logEntry.ToXElement());
+			}
+
+			logDoc.Save(buildLogFile);
+		}
+	}
+}
