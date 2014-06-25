794f87846e5a1c0fcb4fc7c042536a1623d47653
carl.staub <carl.staub@localhost>
2014-06-25 11:13:23 -0700

    Creating 7.6.1

--- dependencymerge2094.ps1
+++ dependencymerge2094.ps1
@@ -1,5 +1,4 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 
-& $scriptPath/updatereqs2094.ps1 
 & $msbuild /t:DependencyMerge_2094 $scriptPath\buildtools.proj
