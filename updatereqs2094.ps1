0302c167ce6b1e4fafef556d82b2dc57a92667c2
carl.staub <carl.staub@localhost>
2014-06-26 02:50:28 -0700

    @merge

--- updatereqs2094.ps1
+++ updatereqs2094.ps1
@@ -1,6 +1,6 @@
 $interactive = '/p:Interactive=false;'
 $customernumber = 'CustomerNumber=2094;'
-$branches = 'Branches="REQ011;REQ013;REQ016;REQ032;REQ033;REQ035;REQ036;REQ044;REQ049;REQ064;REQ081;REQ116;REQ126;REQ127;REQ129;REQ141;REQ145;REQ147;REQ177;REQ180"'
+$branches = 'Branches=""'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "c:\windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 & $msbuild /t:UpdateReqs $interactive$customernumber$branches $scriptPath\buildtools.proj
