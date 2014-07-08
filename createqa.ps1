94ebd22a3c708db125c77abd5b34460315273cf4
carl.staub <carl.staub@localhost>
2014-07-07 19:31:08 -0700

    Tweaking Protect

---
+++ createqa.ps1
@@ -0,0 +1,14 @@
+$interactive = '/p:Interactive=true;'
+
+$args | foreach { if($_ -eq '--non-interactive'){
+      	$interactive = '/p:Interactive=false;'
+      }
+}
+
+$customernumber = 'CustomerNumber=2095;'
+$branches = 'Branches="REQ005;REQ022;REQ006;REQ007;REQ015"'
+$buildTag = 'BuildTag=QA;'
+$baseTag = 'BaseTag=Events;'
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+& msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
