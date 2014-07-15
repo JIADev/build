4031e1e9620b733fe6f6610833225a1b575c49cc
carl.staub <carl.staub@localhost>
2014-07-14 17:39:43 -0700

    Adding REQ 5 and 22 to 7.6.1

--- createqa.ps1
+++ createqa.ps1
@@ -6,9 +6,9 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2095;'
-$branches = 'Branches="REQ005;REQ022;REQ011;REQ006;REQ007;REQ014;REQ015;REQ004;REQ003"'
+$branches = 'Branches="Config_7.6.1;REQ005;REQ022;REQ011;REQ006;REQ007;REQ015;REQ004"'
 $buildTag = 'BuildTag=QA;'
-$baseTag = 'BaseTag=Events;'
+$baseTag = 'BaseTag=Events_7.6.1;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 & msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
