0a68c65dc0581aa3d967d20b5eaa03bfe175d3d6
carl.staub <carl.staub@localhost>
2014-07-21 10:35:10 -0700

    Creating build for enrollment work

--- createqa.ps1
+++ createqa.ps1
@@ -6,7 +6,7 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2095;'
-$branches = 'Branches="REQ005;REQ022;REQ011;REQ006;REQ007;REQ014;REQ015;REQ004;REQ003;Config"'
+$branches = 'Branches="REQ002;REQ005;REQ022;REQ011;REQ006;REQ007;REQ014;REQ015;REQ004;REQ003;Config"'
 $buildTag = 'BuildTag=QA;'
 $baseTag = 'BaseTag=Events;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
