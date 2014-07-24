670bee967958f5f2b2e2f2a45f14f5359ccab846
tim.shattuck <tim.shattuck@localhost>
2014-07-24 16:38:33 -0700

    Removed 2095 builds except REQ001

---
+++ createtrack2.ps1
@@ -0,0 +1,15 @@
+$interactive = '/p:Interactive=true;'
+
+$args | foreach { if($_ -eq '--non-interactive'){
+      	$interactive = '/p:Interactive=false;'
+      }
+}
+
+$customernumber = 'CustomerNumber=2094;'
+$branches = 'Branches="REQ141;REQ129;REQ132;REQ127;REQ126;"'
+$buildTag = 'BuildTag=TRK2;'
+$baseTag = 'BaseTag=7.6.0;'
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+& msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
+
