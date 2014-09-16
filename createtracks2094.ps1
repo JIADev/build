3579299e3ac495c73c5a161d2fc8edc73c73c03e
carl.staub <carl.staub@localhost>
2014-09-16 12:18:49 -0700

    Fixing branch name

---
+++ createtracks2094.ps1
@@ -0,0 +1,7 @@
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+& $scriptPath\createtrack1.ps1 --non-interactive
+& $scriptPath\createtrack2.ps1 --non-interactive
+& $scriptPath\createtrack3.ps1 --non-interactive
+& $scriptPath\createtrack4.ps1 --non-interactive
+& $scriptPath\createtrack5.ps1 --non-interactive
+& $scriptPath\createrc1.ps1
