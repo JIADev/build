9644334d60ab1d333d3162eef2ddb38e3aafe555
carl.staub <carl.staub@localhost>
2014-09-22 11:19:30 -0700

    Changing prod tags

--- create2094rc3.ps1
+++ create2094rc3.ps1
@@ -8,7 +8,7 @@ $args | foreach { if($_ -eq '--non-interactive'){
 $customernumber = 'CustomerNumber=2094;SuppressPrefix="true";'
 $branches = 'Branches="2094_REQ065;2094_REQ070;2094_REQ066"'
 $buildTag = 'BuildTag=2094_RC3;'
-$baseTag = 'BaseTag=2094_RC2.1;'
+$baseTag = 'BaseTag=2094_RC3a;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 & msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
