9345e4c3072025fd7dc8990535009e4aacc5d2a3
carl.staub <carl.staub@localhost>
2014-10-08 00:28:52 -0700

    Reconfigure for new DB server.

---
+++ Code/DeleteIfEmpty.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace j6.BuildTools
+{
+	class Program
+	{
+		private static int Main(string[] args)
+		{
+			var files = args;
+			if (files.Length == 0)
+				files = Directory.GetFiles(Environment.CurrentDirectory, "*.*");
+
+			foreach (var file in files)
+			{
+				var info = new FileInfo(file);
+				if(!info.Exists)
+					continue;
+				if(info.Length == 0)
+					info.Delete();
+			}
+			return 0;
+		}
+	}
+}
---
+++ createtrack4.ps1
@@ -0,0 +1,14 @@
+$interactive = '/p:Interactive=true;'
+
+$args | foreach { if($_ -eq '--non-interactive'){
+      	$interactive = '/p:Interactive=false;'
+      }
+}
+
+$customernumber = 'CustomerNumber=2094;'
+$branches = 'Branches="REQ016;REQ060;REQ193;REQ177;REQ002;REQ006;REQ005"'
+$buildTag = 'BuildTag=TRK4;'
+$baseTag = 'BaseTag=7.6.0;'
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+& msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
