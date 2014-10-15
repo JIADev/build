26f3bb7b09df3163963521faadfd6fb973468801
carl.staub <carl.staub@localhost>
2014-10-14 21:55:55 -0700

    Making demo build

--- create2095rc2.ps1
+++ create2095rc2.ps1
@@ -6,8 +6,8 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2095;SuppressPrefix="true";'
-$branches = 'Branches="2095_REQ024"'
-$buildTag = 'BuildTag=2095_RC2;'
+$branches = 'Branches="2095_REQ042;2095_REQ039;2095_REQ019_7.6.0;2095_REQ008"'
+$buildTag = 'BuildTag=2095_RC2-demo;'
 $baseTag = 'BaseTag=2095_RC1.2;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
