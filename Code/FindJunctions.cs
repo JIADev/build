a358dc2980f42b29f46d9db512585c658507c32d
carl.staub <carl.staub@localhost>
2014-10-14 21:55:55 -0700

    Making demo build

---
+++ Code/FindJunctions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace j6.BuildTools
+{
+	class Program
+	{
+		private static int Main(string[] args)
+		{
+			foreach (var arg in args)
+			{
+				var errorLevel = FindJunctions(arg);
+				if (errorLevel > 0)
+					return errorLevel;
+			}
+
+			return args.Length == 0 ? FindJunctions() : 0;
+		}
+
+		private static int FindJunctions()
+		{
+			return FindJunctions(Environment.CurrentDirectory);
+		}
+
+		private static int FindJunctions(string directory)
+		{
+			try
+			{
+				var errors = FindJunctions(new DirectoryInfo(directory));
+				return errors.Any() ? 5 : 0;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				return 255;
+			}
+			
+		}
+
+		private static Dictionary<string, Exception> FindJunctions(DirectoryInfo dir)
+		{
+			var errors = new Dictionary<string, Exception>();
+
+			var subdirs = dir.GetDirectories();
+
+			foreach (var subdir in subdirs.Where(d => !d.Name.StartsWith(".hg")))
+			{
+				var isJunction = subdir.Attributes.HasFlag(FileAttributes.ReparsePoint);
+				if (isJunction)
+				{
+					Console.WriteLine(subdir.FullName);
+					continue;
+				}
+				errors = errors.Union(FindJunctions(subdir)).ToDictionary(e => e.Key, e => e.Value);
+			}
+			return errors;
+		}
+	}
+}
---
+++ createtrack3.ps1
@@ -0,0 +1,15 @@
+$interactive = '/p:Interactive=true;'
+
+$args | foreach { if($_ -eq '--non-interactive'){
+      	$interactive = '/p:Interactive=false;'
+      }
+}
+
+$customernumber = 'CustomerNumber=2094;'
+$branches = 'Branches="REQ013;REQ016;REQ177;REQ038;REQ146;REQ147;REQ064;REQ145;REQ011;REQ180;REQ116"'
+$buildTag = 'BuildTag=TRK3;'
+$baseTag = 'BaseTag=7.6.0;'
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+& msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
+
