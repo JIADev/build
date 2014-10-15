0b1e19bb9490514bd6c653658217a891ec8652ab
carl.staub <carl.staub@localhost>
2014-10-15 01:12:25 -0700

    Fixing syntax

--- create2095rc2.ps1
+++ create2095rc2.ps1
@@ -12,3 +12,8 @@ $baseTag = 'BaseTag=2095_RC1.2;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 & msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
+
+$branches = 'Branches="2095_REQ024"'
+$buildTag = 'BuildTag=2095_RC2;'
+$baseTag = 'BaseTag=2095_RC2-demo;'
+& msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
