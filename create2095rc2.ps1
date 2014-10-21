135596ee9e0965009448274a6c44d115ef3a2975
carl.staub <carl.staub@localhost>
2014-10-20 19:15:27 -0700

    Adding REQ 19 to RC2 build.

--- create2095rc2.ps1
+++ create2095rc2.ps1
@@ -13,7 +13,7 @@ $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 & msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
 
-$branches = 'Branches="2095_REQ024"'
+$branches = 'Branches="2095_REQ024;2095_REQ019_7.6.0"'
 $buildTag = 'BuildTag=2095_RC2;'
 $baseTag = 'BaseTag=2095_RC2-demo;'
 & msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
