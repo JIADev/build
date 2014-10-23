b017a11a7c9c2e9e6660d2695eca80bbc5228bca
carl.staub <carl.staub@localhost>
2014-10-23 12:09:01 -0700

    Updating create build script

--- create2095rc2.ps1
+++ create2095rc2.ps1
@@ -6,14 +6,9 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2095;SuppressPrefix="true";'
-$branches = 'Branches="2095_REQ042;2095_REQ039;2095_REQ008;2095_REQ010;2095_REQ029;2095_REQ019_7.6.0"'
-$buildTag = 'BuildTag=2095_RC2-demo;'
-$baseTag = 'BaseTag=2095_RC1.2;'
+$branches = 'Branches=""'
+$buildTag = 'BuildTag=2095_RC2.2;'
+$baseTag = 'BaseTag=2095_RC2.1;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
-& msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
-
-$branches = 'Branches="2095_REQ024"'
-$buildTag = 'BuildTag=2095_RC2;'
-$baseTag = 'BaseTag=2095_RC2-demo;'
 & msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
