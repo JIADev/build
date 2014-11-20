5686597df0e8faddfa3a8e33d9da1bc5c36ccfaa
carl.staub <carl.staub@localhost>
2014-11-19 16:00:33 -0800

    Adding 213 and 184 to RC 3.2 build

--- create2094rc3.2.ps1
+++ create2094rc3.2.ps1
@@ -7,9 +7,9 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2094;SuppressPrefix="true";'
-$branches = 'Branches="2094_REQ301.B"'
+$branches = 'Branches="2094_REQ213;2094_REQ301.B;2094_REQ184"'
 $buildTag = 'BuildTag=2094_RC3.2;'
-$baseTag = 'BaseTag=2095_2094_Merge1;'
+$baseTag = 'BaseTag=2094_7.6.4;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 & msbuild /t:CreateBuild $initsourcerepo$interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
