c445bd2caca64d840d77c8a9abe46b7b17ca4fb5
carl.staub <carl.staub@localhost>
2014-11-22 10:32:30 -0800

    Updating create 2094 RC3.2 build script

--- create2094rc3.2.ps1
+++ create2094rc3.2.ps1
@@ -7,7 +7,7 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2094;SuppressPrefix="true";'
-$branches = 'Branches="2094_RC3.2;2094_REQ310.B;2094_REQ213;2094_REQ301.B;2094_REQ184"'
+$branches = 'Branches="2094_RC3.2;2094_REQ310.B;2094_REQ213;2094_REQ301.B;2094_REQ184;2094_REQ164"'
 $buildTag = 'BuildTag=2094_RC3.2;'
 $baseTag = 'BaseTag=2094_7.6.4;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
