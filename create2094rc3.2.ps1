2bba4beae4cf2e552eb33ff29e11f723df2133d5
carl.staub <carl.staub@localhost>
2014-11-26 15:58:32 -0800

    Adding req 002 for 2095

--- create2094rc3.2.ps1
+++ create2094rc3.2.ps1
@@ -7,9 +7,9 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2094;SuppressPrefix="true";'
-$branches = 'Branches="2094_RC3.2;2094_REQ310.B;2094_REQ213;2094_REQ301.B;2094_REQ184;2094_REQ164"'
+$branches = 'Branches="2094_7.6.4;2094_REQ310.B;2094_REQ301.B;2094_REQ184;2094_REQ164"'
 $buildTag = 'BuildTag=2094_RC3.2;'
-$baseTag = 'BaseTag=2094_7.6.4;'
+$baseTag = 'BaseTag=2094_RC3.2;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 & msbuild /t:CreateBuild $initsourcerepo$interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
