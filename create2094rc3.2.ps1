54930e9faa80209ab60069da29d16dc13b1dfbb2
carl.staub <carl.staub@localhost>
2014-12-02 11:45:29 -0800

    Updating REQ list.

--- create2094rc3.2.ps1
+++ create2094rc3.2.ps1
@@ -1,4 +1,4 @@
-$initsourcerepo='/p:InitSourceRepo=active;'
+$initsourcerepo='/p:Preview=false;InitSourceRepo=active;'
 $interactive = 'Interactive=true;'
 
 $args | foreach { if($_ -eq '--non-interactive'){
@@ -7,9 +7,9 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2094;SuppressPrefix="true";'
-$branches = 'Branches="2094_REQ310.4;2094_REQ301.4;2094_REQ184.4;2094_REQ164.4;2094_7.6.4"'
+$branches = 'Branches="2094_REQ310.4;2094_REQ301.4;2094_REQ304.4;2094_REQ184.4;2094_REQ164.4;2094_REQ163.4"'
 $buildTag = 'BuildTag=2094_RC3.2;'
-$baseTag = 'BaseTag=2094_RC3.2;'
+$baseTag = 'BaseTag=2094_7.6.4;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 & msbuild /t:CreateBuild $initsourcerepo$interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
