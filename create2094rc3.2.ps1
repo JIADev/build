56be76ef945749d3654cb828e517fc1a9cf43bb1
carl.staub <carl.staub@localhost>
2014-12-04 12:26:15 -0800

    Changing queue suffixes

--- create2094rc3.2.ps1
+++ create2094rc3.2.ps1
@@ -7,7 +7,7 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2094;SuppressPrefix="true";'
-$branches = 'Branches="2094_REQ213.4;2094_REQ309.4;2094_REQ306.4;2094_REQ308.4;2094_REQ310.4;2094_REQ301.4;2094_REQ304.4;2094_REQ184.4;2094_REQ164.4;2094_REQ163.4"'
+$branches = 'Branches="2094_RC3.2;2094_REQ213.4;2094_REQ309.4;2094_REQ306.4;2094_REQ308.4;2094_REQ310.4;2094_REQ301.4;2094_REQ304.4;2094_REQ184.4;2094_REQ164.4;2094_REQ163.4"'
 $buildTag = 'BuildTag=2094_RC3.2;'
 $baseTag = 'BaseTag=2094_7.6.4;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
