3b7e4434c1a1c56280fce8b4ad31a9c79f4586fe
carl.staub <carl.staub@localhost>
2014-12-24 01:17:41 -0800

    Adding missing file

---
+++ create2094rc3.3.ps1
@@ -0,0 +1,15 @@
+$initsourcerepo='/p:TrashBranch=trashcan;Preview=false;InitSourceRepo=active;'
+$interactive = 'Interactive=true;'
+
+$args | foreach { if($_ -eq '--non-interactive'){
+      	$interactive = 'Interactive=false;'
+      }
+}
+
+$customernumber = 'CustomerNumber=2094;SuppressPrefix="true";'
+$branches = 'Branches="2094_REQ216.4;2094_CR141.001"'
+$buildTag = 'BuildTag=2094_RC3.3;'
+$baseTag = 'BaseTag=7.6.5;'
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+& msbuild /t:CreateBuild $initsourcerepo$interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
