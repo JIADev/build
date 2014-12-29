a8f9e1465f4657b01ea6521688f3b66c7a4a11ee
carl.staub <carl.staub@localhost>
2014-12-29 09:45:08 -0800

    Adding RC3.2 build back

--- create2094rc3.3.ps1
+++ create2094rc3.3.ps1
@@ -7,7 +7,7 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2094;SuppressPrefix="true";'
-$branches = 'Branches="2094_REQ216.4;2094_CR141.001"'
+$branches = 'Branches="2094_REQ216.4;2094_CR141.001;2094_REQ058.4"'
 $buildTag = 'BuildTag=2094_RC3.3;'
 $baseTag = 'BaseTag=7.6.5;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
