ebdc7922bb225f9d4d80eb66023aa55e3ddbb1e9
carl.staub <carl.staub@localhost>
2014-12-29 09:45:08 -0800

    Adding RC3.2 build back

--- create2094rc3.2.ps1
+++ create2094rc3.2.ps1
@@ -7,9 +7,9 @@ $args | foreach { if($_ -eq '--non-interactive'){
 }
 
 $customernumber = 'CustomerNumber=2094;SuppressPrefix="true";'
-$branches = 'Branches="2094_RC3.2;2094_REQ213.4;2094_REQ309.4;2094_REQ306.4;2094_REQ308.4;2094_REQ310.4;2094_REQ301.4;2094_REQ304.4;2094_REQ184.4;2094_REQ164.4;2094_REQ163.4"'
+$branches = 'Branches="2094_REQ216.4"'
 $buildTag = 'BuildTag=2094_RC3.2;'
-$baseTag = 'BaseTag=2094_7.6.4;'
+$baseTag = 'BaseTag=7.6.5;'
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 & msbuild /t:CreateBuild $initsourcerepo$interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
