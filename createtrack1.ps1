d99ecd07fcc5e10287aec7c18c7f87181ab9ff1f
tim.shattuck <tim.shattuck@localhost>
2014-12-30 09:56:54 -0800

    Updated que2

---
+++ createtrack1.ps1
@@ -0,0 +1,14 @@
+$interactive = '/p:Interactive=true;'
+
+$args | foreach { if($_ -eq '--non-interactive'){
+      	$interactive = '/p:Interactive=false;'
+      }
+}
+
+$customernumber = 'CustomerNumber=2094;'
+$branches = 'Branches="REQ033;REQ036;REQ035;REQ039;REQ029;REQ031;REQ017"'
+$buildTag = 'BuildTag=TRK1;'
+$baseTag = 'BaseTag=7.6.0;'
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+& msbuild /t:CreateBuild $interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
