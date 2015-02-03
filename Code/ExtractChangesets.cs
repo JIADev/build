b55efdedc7aeaabec3040773dfdbb28876afc8dd
tshattuck <tshattuck@localhost>
2015-02-03 15:23:32 -0800

    Updated xDocument name

--- Code/ExtractChangesets.cs
+++ Code/ExtractChangesets.cs
@@ -94,7 +94,7 @@ namespace j6.BuildTools
 					}
 					docList.Add(xDoc);
 				}
-				firstDoc.AddFirst(new XProcessingInstruction("xml-stylesheet", "type=\"text/xsl\" href=\"http://jia-build1.jenkon.com/ccnet/xsl/changelog.xsl\""));
+				docList.AddFirst(new XProcessingInstruction("xml-stylesheet", "type=\"text/xsl\" href=\"http://jia-build1.jenkon.com/ccnet/xsl/changelog.xsl\""));
 			}
 			if (docList.Count < 1)
 				return;
@@ -103,7 +103,6 @@ namespace j6.BuildTools
 			
 			if (docList.Count == 1)
 			{
-				//firstDoc.AddFirst(new XProcessingInstruction("xml-stylesheet", "type=\"text/xsl\" href=\"http://jia-build1.jenkon.com/ccnet/xsl/changelog.xsl\""));
 				firstDoc.Save(outputFile);
 				return;
 			}
@@ -114,7 +113,6 @@ namespace j6.BuildTools
 				var docElements = currentDoc.XPathSelectElements("/log/logentry");
 				firstDoc.XPathSelectElement("/log").Add(docElements);
 			}
-			//firstDoc.AddFirst(new XProcessingInstruction("xml-stylesheet", "type=\"text/xsl\" href=\"http://jia-build1.jenkon.com/ccnet/xsl/changelog.xsl\""));
 			firstDoc.Save(outputFile);
 		}
 
