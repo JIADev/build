5793679254945f76e303ebe03a9e3b25faf13eaa
tshattuck <tshattuck@localhost>
2015-04-07 09:33:35 -0700

    Removed track & moved to jia-build1 for packaging build for qa

---
+++ create2094rc3.3.ps1
@@ -0,0 +1,15 @@
+$initsourcerepo='/p:TrashBranch=trashcan;Preview=false;InitSourceRepo=active;'
+$interactive = 'Interactive=true;'
+
+$args | foreach { if($_ -eq '--non-interactive'){
+      	$interactive = 'Interactive=false;'
+      }
+}
+
+$customernumber = 'CustomerNumber=2094;SuppressPrefix="true";'
+$branches = 'Branches="2095_RC3a;2094_CR141.001;2094_REQ058.4;2094_CR039.001;2094_REQ213.5"'
+$buildTag = 'BuildTag=2094_RC3.3;'
+$baseTag = 'BaseTag=2094_RC3.2;'
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+& msbuild /t:CreateBuild $initsourcerepo$interactive$customernumber$buildTag$baseTag$branches $scriptPath\buildtools.proj
