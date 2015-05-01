1d2ca49a6fb039bbe7b750f3fe0a2e27f8283d1d
tshattuck <tshattuck@localhost>
2015-05-01 10:19:02 -0700

    updated for SQL 2014 support

---
+++ Code/CleanProjectFiles.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace j6.BuildTools
+{
+	class Program
+	{
+		private static int Main(string[] args)
+		{
+			var outputFile = args.Length > 1 ? args[1] : null;
+			var fileList = new List<string>();
+			if (args.Length > 0)
+				fileList.Add(args[0]);
+			else
+			{
+				fileList.AddRange(GetCsProjFiles());
+			}
+			var filesModified = fileList.Sum(f => RemoveDuplicateEntries(f, outputFile));
+			Console.WriteLine(string.Format("Modified {0} project files", filesModified));
+			var featureExe = @"Core\boot\feature.exe";
+			if (File.Exists(featureExe))
+			{
+				BuildSystem.RunProcess(featureExe, "cleanpatches", Environment.CurrentDirectory);
+			}
+			return 0;
+		}
+
+		private static IEnumerable<string> GetCsProjFiles()
+		{
+			return Directory.GetFiles(Environment.CurrentDirectory, "*.csproj", SearchOption.AllDirectories);
+		}
+
+		private static int RemoveDuplicateEntries(string fileName, string outputFile = null)
+		{
+			var xDocument = XDocument.Load(fileName);
+			var root = xDocument.Root;
+			
+			if (root == null)
+				return 0;
+
+			var badNodes =
+				root.Elements()
+				    .Where(
+					    n =>
+					    n.Name.LocalName == "Import" &&
+					    n.Attributes()
+					     .Any(
+						     a =>
+						     a.Name == "Project" &&
+						     a.Value.Equals(@"$(SolutionDir)\.nuget\NuGet.targets", StringComparison.InvariantCulture)))
+				    .Union(
+					    root.Elements()
+					        .Where(
+						        n =>
+						        n.Name.LocalName == "Target" &&
+						        n.Attributes()
+						         .Any(
+							         a =>
+							         a.Name == "Name" && a.Value.Equals("EnsureNuGetPackageBuildImports", StringComparison.InvariantCulture))
+									 &&
+								n.Attributes().Any(
+							         a =>
+							         a.Name == "BeforeTargets" && a.Value.Equals("PrepareForBuild", StringComparison.InvariantCulture)))).ToArray();
+
+			var duplicateNodes =
+				root.Elements()
+					.Where(n => n.Name.LocalName == "ItemGroup")
+					.SelectMany(i => i.Elements())
+					.GroupBy(c => c.Name.LocalName).Where(c => c.Attributes().Any(a => a.Name.LocalName == "Include"))
+					.ToDictionary(c => c.Key, c => c.GroupBy(d => d.Attributes().Single(e => e.Name.LocalName == "Include").Value).Where(f => f.Count() > 1)
+						.ToDictionary(f => f.Key, f => f.ToArray())).SelectMany(f => f.Value.SelectMany(g => g.Value.Skip(1))).ToArray();
+			
+			var fileModified = false;
+
+			foreach (var bad in badNodes)
+			{
+				bad.Remove();
+				fileModified = true;
+			}
+
+			foreach (var dup in duplicateNodes)
+			{
+				dup.Remove();
+				fileModified = true;
+			}
+
+			if (!fileModified)
+				return 0;
+
+			xDocument.Save(outputFile ?? fileName);
+			
+			return 1;
+		}
+	}
+}
---
+++ Code/RunProcess.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace j6.BuildTools
+{
+	public static class BuildSystem
+	{
+		public static string RunProcess(string process,
+			string args,
+			string workingDirectory,
+			Dictionary<string, string> extraEnvVariables = null,
+			int timeoutSeconds = 0)
+		{
+			var startInfo =
+				new ProcessStartInfo
+				{
+					FileName = process,
+					UseShellExecute = false,
+					RedirectStandardError = true,
+					RedirectStandardOutput = true,
+					Arguments = args,
+					WorkingDirectory = workingDirectory,
+					StandardErrorEncoding = Encoding.UTF8,
+					StandardOutputEncoding = Encoding.UTF8,
+				};
+			foreach (var extraEnvVariable in extraEnvVariables ?? new Dictionary<string, string>())
+			{
+				startInfo.EnvironmentVariables[extraEnvVariable.Key] = extraEnvVariable.Value;
+			}
+			var proc = new Process
+			{
+				StartInfo = startInfo
+			};
+
+			var outputString = new StringWriter();
+            Trace.WriteLine(string.Format("Starting: {0} with arguments: {1}", startInfo.FileName, startInfo.Arguments));
+			proc.Start();
+			var errorBuilder = new StringBuilder();
+
+			var outputWriter = new Thread(() =>
+			{
+				string line;
+				lock (proc)
+					while ((line = proc.StandardOutput.ReadLine()) != null)
+					{
+                        Console.WriteLine(line);
+						outputString.WriteLine(line);
+					}
+			});
+			var errorWriter = new Thread(() =>
+			{
+				string line;
+				lock (proc)
+					while ((line = proc.StandardError.ReadLine()) != null)
+					{
+                        Console.Error.WriteLine(line);
+						errorBuilder.AppendLine(line);
+					}
+			});
+			outputWriter.Start();
+			errorWriter.Start();
+			if (!string.IsNullOrEmpty(errorBuilder.ToString()))
+				throw new Exception(errorBuilder.ToString());
+
+			if (timeoutSeconds == 0)
+				proc.WaitForExit();
+			else
+				proc.WaitForExit(timeoutSeconds*1000);
+			
+			if (!proc.HasExited)
+			{
+				proc.Kill();
+				throw new TimeoutException(string.Format("Process {0} timed out after {1} seconds", startInfo.FileName, timeoutSeconds));
+			}
+			
+			outputWriter.Join();
+			errorWriter.Join();
+
+			if (proc.ExitCode != 0)
+				throw new Exception(
+					string.Format("{0} {1}: {2}", process, args, proc.ExitCode));
+
+			return outputString.ToString();
+		}
+
+		public static void PowerShell(string command, string workingDirectory = ".", bool encodeCommand = false)
+		{
+			if (encodeCommand)
+			{
+				command = Convert.ToBase64String(Encoding.Unicode.GetBytes(command));
+			}
+			RunProcess("powershell",
+				string.Format("-{1} \"{0}\"", command, encodeCommand ? "EncodedCommand" : "Command"),
+					workingDirectory);
+		}
+	}
+}
