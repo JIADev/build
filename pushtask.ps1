81aa02d1001c677a428dc982eed29114aff62555
carl.staub <carl.staub@localhost>
2015-05-21 01:38:31 -0700

    New version

--- pushtask.ps1
+++ pushtask.ps1
@@ -1,6 +1,9 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
-& $msbuild /t:UpdateBuildToolsRepo $scriptPath\buildtools.proj
+. "$scriptPath\mercurialTasks.ps1"
+
+updateBuildTools
+
 Write-Host "Version 1"
 
 $ongoingBranch = '2095_QA2015.05.19'
@@ -22,25 +25,15 @@ if($LastExitCode -ne 0) {
 	Write-Host "Closing task branch failed"
 	Exit
 }
-$pinfo = New-Object System.Diagnostics.ProcessStartInfo
-$pinfo.FileName = "hg.exe"
-$pinfo.RedirectStandardOutput = $true
-$pinfo.UseShellExecute = $false
-$pinfo.Arguments = "branch"
-$currentDir = Convert-Path .
-$pinfo.WorkingDirectory = $currentDir
-$p = New-Object System.Diagnostics.Process
-$p.StartInfo = $pinfo
-$p.Start() | Out-Null
-$p.WaitForExit()
-$currentBranch = $p.StandardOutput.ReadToEnd()
+$currentBranch = getCurrentBranch
 
-if($p.ExitCode -ne 0) { 
+if($currentBranch -eq '') { 
 	Write-Host "Cannot determine my branch"
 	Exit
 }
 
 Write-Host "Updating to branch $ongoingBranch"
+
 & hg up $ongoingBranch
 if($LastExitCode -ne 0) { 
 	Write-Host "Cannot update to $ongoingBranch"
@@ -55,3 +48,4 @@ if($LastExitCode -ne 0) {
 }
 
 & Write-Host "hg push --new-branch"
+
