642cad298a58bbfee24a5c4e8115b93ca2177e89
carl.staub <carl.staub@localhost>
2015-05-21 02:26:58 -0700

    Need MSBuild for revertall if selected

--- starttask.ps1
+++ starttask.ps1
@@ -1,4 +1,5 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 . "$scriptPath\mercurialTasks.ps1"
 
 Write-Host "Updating $scriptPath"
