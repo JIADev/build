a20d76b4bb54274f7ac8bb140fa0411cf206325c
carl.staub <carl.staub@localhost>
2015-05-21 17:02:40 -0700

    Revert revertall

--- Code/RevertAll.cs
+++ Code/RevertAll.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
-// ReSharper disable RedundantStringFormatCall
 namespace j6.BuildTools
 {
 	class Program
 	{
+		static readonly Regex ChangesetRegex = new Regex("^changeset:.*$", RegexOptions.Multiline);
 		private static bool _verbose;
 
 		private static int Main(string[] args)
@@ -22,24 +29,35 @@ namespace j6.BuildTools
 					return 2;
 				}
 				
+				var hgExe = "hg";
+				RevertAll(hgExe, repoRoot);
+
+				var hgIgnoreFile = repoRoot.GetFiles(".hgignore").Where(f => f.Exists && f.Name.Equals(".hgignore", StringComparison.InvariantCultureIgnoreCase)).Select(f => f.FullName).SingleOrDefault();
+				var tmpIgnoreFile = Path.Combine(repoRoot.FullName, string.Format("{0}.hgignore", Guid.NewGuid()));
+
+				if(hgIgnoreFile != null)
+					File.Move(hgIgnoreFile, tmpIgnoreFile);
+
 				var junctionsDeleted = DeleteJunctions(repoRoot);
 				Console.WriteLine("Deleted {0} junctions.", junctionsDeleted);
 
-				var infos = repoRoot.GetFileSystemInfos().Where(i => !i.Name.Equals(".hg")).ToArray();
+				var files = GetHgStatFiles(hgExe, repoRoot, tmpIgnoreFile);
 
-				foreach (var info in infos)
+				if (hgIgnoreFile != null)
 				{
-					if (_verbose)
-						Console.WriteLine(string.Format("Deleting {0}", info));
-					
-					var dirInfo = info as DirectoryInfo;
-					
-					if (dirInfo != null)
-						dirInfo.Delete(true);
-					else
-						info.Delete();
+					File.Move(tmpIgnoreFile, hgIgnoreFile);
 				}
-				Console.WriteLine("Deleted {0} files and directories.", infos.Length);
+				var fileCount = 0;
+				foreach (var file in files.Where(f => f.Exists))
+				{
+					if(_verbose)
+						Console.WriteLine(string.Format("Deleting {0}", file));
+					file.Delete();
+					fileCount++;
+				}
+				Console.WriteLine(string.Format("Deleted {0} files.", fileCount));
+				var emptyDirectories = DeleteEmptyDirectories(repoRoot);
+				Console.WriteLine(string.Format("Deleted {0} empty directories.", emptyDirectories));
 			}
 			catch (Exception ex)
 			{
@@ -61,6 +79,27 @@ namespace j6.BuildTools
 			return directory.GetDirectories().Sum(dir => DeleteJunctions(dir));
 		}
 
+		private static int DeleteEmptyDirectories(DirectoryInfo directory)
+		{
+			var emptyDirectoriesDeleted = directory.GetDirectories().Sum(dir => DeleteEmptyDirectories(dir));
+			
+			if (!directory.GetFileSystemInfos().Any())
+			{
+				if(_verbose)
+					Console.WriteLine(string.Format("Deleting empty directory {0}", directory));
+				directory.Delete(false);
+				emptyDirectoriesDeleted++;
+			}
+			return emptyDirectoriesDeleted;
+		}
+
+		private static void RevertAll(string hgExe, DirectoryInfo directory)
+		{
+			if(_verbose)
+				Console.WriteLine("Reverting all files under " + directory.FullName);
+			BuildSystem.RunProcess(hgExe, "revert --all --no-backup", directory.FullName);
+		}
+
 		private static DirectoryInfo FindRepoRoot(DirectoryInfo directory)
 		{
 			var hasHgDirectory =
@@ -72,6 +111,28 @@ namespace j6.BuildTools
 				return FindRepoRoot(directory.Parent);
 
 			return null;
+		}
+		
+		private static FileInfo[] GetHgStatFiles(string hgExe, DirectoryInfo directory, string tempIgnoreFile)
+		{
+			var output = BuildSystem.RunProcess(hgExe, "status", directory.FullName);
+			var currentAssembly = System.Reflection.Assembly.GetExecutingAssembly().Location;
+			var files = output.Split(new [] { '\n' }, StringSplitOptions.RemoveEmptyEntries)
+							.Select(s => s.TrimStart(new [] { ' ', '?' }).Trim())
+							.Select(s =>
+								{
+									try
+									{
+										var file = new FileInfo(s);
+										return file;
+									}
+									catch
+									{
+										return null;
+									}
+								})
+							.Where(f => f != null && f.Exists && f.FullName != tempIgnoreFile && !f.FullName.Equals(currentAssembly, StringComparison.InvariantCultureIgnoreCase)).ToArray();
+			return files;
 		}
 	}
 }
