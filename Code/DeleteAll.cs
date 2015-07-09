72b7502c6cfc7254503fc377e532ea0016263118
tshattuck <tshattuck@localhost>
2015-07-09 15:22:11 -0700

    Created build branch for 2094_CR009

---
+++ Code/DeleteAll.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+// ReSharper disable RedundantStringFormatCall
+namespace j6.BuildTools
+{
+	class Program
+	{
+		private static bool _verbose;
+		enum FileType
+		{
+			File,
+			Directory,
+			Junction
+		}
+		private static int Main(string[] args)
+		{
+			try
+			{
+				_verbose = args.Any(a => a.Equals("--verbose", StringComparison.InvariantCultureIgnoreCase));
+
+				var repoRoot = FindRepoRoot(new DirectoryInfo(Environment.CurrentDirectory));
+				if (repoRoot == null)
+				{
+					Console.WriteLine("Must be in a mercurial repository.");
+					return 2;
+				}
+
+				var deleted = DeleteDirectories(repoRoot, new[] {".hg"});
+
+				Console.WriteLine("Deleted {0} files, {1} junctions, and {2} directories.",
+				                  deleted[FileType.File],
+				                  deleted[FileType.Junction],
+								  deleted[FileType.Directory]);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				return 3;
+			}
+			return 0;
+		}
+
+		private static Dictionary<FileType, int> DeleteDirectories(DirectoryInfo directory, string[] exclude)
+		{
+			var returnValue = new Dictionary<FileType, int>
+				{
+					{ FileType.File, 0 },
+					{ FileType.Junction, 0 },
+					{ FileType.Directory, 0 }
+				};
+			foreach (var fsInfo in directory.GetFileSystemInfos())
+			{
+				if(exclude != null && exclude.Contains(fsInfo.Name, StringComparer.InvariantCultureIgnoreCase))
+					continue;
+				
+				var file = fsInfo as FileInfo;
+				var subDir = fsInfo as DirectoryInfo;
+				
+				if (file != null && file.Exists)
+				{
+					file.Delete();
+					if(_verbose)
+						Console.WriteLine(string.Format("Deleted {0}", file.FullName));
+					returnValue[FileType.File]++;
+				}
+
+				if (subDir == null || !subDir.Exists) continue;
+
+				if (subDir.Attributes.HasFlag(FileAttributes.ReparsePoint))
+				{
+					subDir.Delete();
+					if (_verbose)
+						Console.WriteLine(string.Format("Deleted Junction {0}", subDir));
+					returnValue[FileType.Junction]++;
+					continue;
+				}
+
+				var subResults = DeleteDirectories(subDir, exclude);
+				returnValue[FileType.File] += subResults[FileType.File];
+				returnValue[FileType.Junction] += subResults[FileType.Junction];
+				returnValue[FileType.Directory] += subResults[FileType.Directory];
+			}
+			if (!directory.GetFileSystemInfos().Any())
+			{
+				directory.Delete(false);
+				if (_verbose)
+					Console.WriteLine(string.Format("Deleted {0}", directory.FullName));
+				returnValue[FileType.Directory]++;
+			}
+			return returnValue;
+		}
+
+		private static DirectoryInfo FindRepoRoot(DirectoryInfo directory)
+		{
+			var hasHgDirectory =
+				directory.GetDirectories(".hg").Any(d => d.Name.Equals(".hg", StringComparison.InvariantCultureIgnoreCase));
+			if (hasHgDirectory)
+				return directory;
+
+			if (directory.Parent != null && directory.Parent.Exists)
+				return FindRepoRoot(directory.Parent);
+
+			return null;
+		}
+	}
+}
---
+++ pushtask.ps1
@@ -0,0 +1,79 @@
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+. "$scriptPath\mercurialTasks.ps1"
+. "$scriptPath\startGraftCommon.ps1"
+
+Write-Host "Updating $scriptPath"
+$updateSuccess = updateBuildTools
+
+$noComment = 'true'
+$args | foreach {
+      $noComment = 'false'
+}
+
+if($noComment -eq 'false') {
+	& hg ci -m "$args"
+	if($LastExitCode -ne 0) { 
+		Write-Host "Commit failed"
+		Exit
+	}
+}
+$currentBranch = getCurrentBranch
+
+if($currentBranch -eq '') { 
+	Write-Host "Cannot determine my branch"
+	Exit
+}
+
+$customerNumber = $currentBranch.Substring(0, $currentBranch.IndexOf('_'))
+
+if($customerNumber -eq '') {
+	$customerNumber = Read-Host "Customer Number? (i.e. 2094, 2095, 2096)"
+}
+
+validateCustomer $customerNumber
+$ongoingBranch = $pushTaskBranches[[string]$customerNumber]
+
+Write-Host "Closing branch $currentBranch"
+& hg ci -m "Completing task @build" --close-branch
+
+if($ongoingBranch) {
+if($ongoingBranch -ne '') {
+Write-Host "Updating to branch $ongoingBranch"
+& hg up $ongoingBranch
+if($LastExitCode -ne 0) { 
+	Write-Host "Cannot update to $ongoingBranch"
+	Exit
+}
+
+Write-Host "Merging $currentBranch to $ongoingBranch"
+& hg merge $currentBranch --tool=internal:merge
+if($LastExitCode -ne 0) { 
+	& hg resolve --all
+	if($LastExitCode -ne 0) { 
+
+		Write-Host "Cannot merge $currentBranch to $ongoingBranch"
+		Exit
+	}
+}
+
+Write-Host "Committing Merge"
+& hg ci -m "@merge $currentBranch"
+}}
+
+$currentDir = Convert-Path .
+
+& hg outgoing
+$confirmation = Read-Host "This will push these changes in $currentDir to the server. (y/n?)"
+if($confirmation -eq 'y') {
+  & hg push --new-branch
+  if($LastExitCode -ne 0) { 
+	Write-Host "Cannot push"
+	Exit
+  }
+} else {
+  Write-Host "Not pushed, updating to $currentBranch"
+  & hg up $currentBranch
+}
+
+$currentBranch = getCurrentBranch
+Write-Host "Current Branch $currentBranch"
