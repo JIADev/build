efec8decd5d0b8df592f5b434aeb6b6fd9b0d45d
carl.staub <carl.staub@localhost>
2015-10-27 09:40:14 -0700

    Fixing encoding for changelog.

--- Code/UpdateStyleSheet.cs
+++ Code/UpdateStyleSheet.cs
@@ -12,19 +12,26 @@ namespace TestCmd
     {
         static void Main(string[] args)
         {
-            string log = "ChangesetLog.xml";
-	    var logInfo = new FileInfo("ChangesetLog.xml");
+	        string inputFileName = args.Length > 0 ? args[0] : "ChangesetLog.xml";
+	        string stylesheet = args.Length > 1 ? args[1] : "changelog.xsl";
+	        string outputFileName = args.Length > 2 ? args[2] : inputFileName;
+
+	    var logInfo = new FileInfo(inputFileName);
+
 	    if(!logInfo.Exists || logInfo.Length == 0)
 	    {
 	       return;
 	    }
 	    
             try
-	    {	    
-	    	    XDocument document = XDocument.Load(log);
-            	    document.AddFirst(new XProcessingInstruction(
-               	    "xml-stylesheet", "type=\"text/xsl\" href=\"changelog.xsl\""));
-            	    document.Save(log);
+            {
+	            XDocument document;
+				using (StreamReader reader = new StreamReader(logInfo.FullName, Encoding.UTF8))
+					document = XDocument.Load(reader);
+	            document.Declaration.Encoding = "utf-8";
+            	document.AddFirst(new XProcessingInstruction(
+               	    "xml-stylesheet", string.Format("type=\"text/xsl\" href=\"{0}\"", stylesheet)));
+            	    document.Save(outputFileName);
 	  }
 	  catch(Exception ex)
 	  {
