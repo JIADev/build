2f7fb55e4a4a6c13f05034d4d1c93e6789ba1905
carl.staub <carl.staub@localhost>
2016-02-04 17:05:13 -0800

    Adding forgotten files

---
+++ Code/GetMergedChangesets.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+
+namespace j6.BuildTools.MsBuildTasks
+{
+	public class GetMergedChangesets : HgTask
+	{
+		[Required]
+		public new string RepoDirectory { get { return base.RepoDirectory; } set { base.RepoDirectory = value; } }
+
+		[Required]
+		public string OriginalChangeset { get; set; }
+
+		[Required]
+		public string NewChangeset { get; set; }
+
+		public string AdditionalArgs { get; set; }
+		
+		public string OutputFile { get; set; }
+
+		public string StyleSheet { get; set; }
+
+		private string _updateToChangeset;
+		public string UpdateToChangeset { get { return string.IsNullOrWhiteSpace(_updateToChangeset) ? NewChangeset : _updateToChangeset; } set { _updateToChangeset = value; } }
+
+		
+		public override bool Execute()
+		{
+			var output = RunHgXmlOutput(string.Format("log --rev \"ancestors('{0}') and !ancestors({1}) {2}\"", NewChangeset,
+			                             OriginalChangeset, AdditionalArgs));
+
+			if (string.IsNullOrWhiteSpace(OutputFile)) return true;
+
+			if (!string.IsNullOrWhiteSpace(StyleSheet))
+				UpdateStyleSheet.AddStyleSheet(output, StyleSheet);
+
+			output.Save(OutputFile);
+			
+			return true;
+		}
+	}
+}
---
+++ Code/HgTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+using Microsoft.Build.Utilities;
+using System.Xml;
+
+namespace j6.BuildTools.MsBuildTasks
+{
+	public abstract class HgTask : Task
+	{
+		public string RepoDirectory { get; set; }
+
+		public string HgExe { get; set; }
+
+		private string _additionalHgArgs;
+		public string AdditionalHgArgs { get { return _additionalHgArgs ?? string.Empty; } set { _additionalHgArgs = value; } }
+
+		protected HgTask()
+		{
+			HgExe = "hg";
+		}
+
+		public string[] RunHgArrayOutput(string args)
+		{
+			return RunHg(args).Split(new [] { Environment.NewLine }, StringSplitOptions.None);
+		}
+
+		public string RunHg(string args)
+		{
+			var arguments = args;
+			if (!string.IsNullOrWhiteSpace(AdditionalHgArgs))
+				arguments = string.Format("{0} {1}", arguments, AdditionalHgArgs);
+			var result = BuildSystem.RunProcess(HgExe, arguments, RepoDirectory, displayStdErr: false, displayStdOut: false);
+			return result;
+		}
+
+		public XDocument RunHgXmlOutput(string args)
+		{
+			using (var reader = new StringReader(RunHg(string.Format("{0} --style=xml --verbose", args))))
+				return XDocument.Load(reader);
+		}
+	}
+}
