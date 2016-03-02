b01f616bd7363245ef4c48c5bc81ab04f781d5ca
carl.staub <carl.staub@localhost>
2016-03-02 10:31:47 -0800

    More support for multiple prod tags

--- Code/ForwardChangesCheck.cs
+++ Code/ForwardChangesCheck.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using Microsoft.Build.Utilities;
 using Microsoft.Build.Framework;
 
 namespace j6.BuildTools.MsBuildTasks
@@ -11,14 +8,37 @@ namespace j6.BuildTools.MsBuildTasks
 	{
 		[Required]
 		public new string RepoDirectory { get { return base.RepoDirectory; } set { base.RepoDirectory = value; } }
+		
 		[Required]
 		public string OriginalChangeset { get; set; }
+		
 		[Required]
 		public string NewChangeset { get; set; }
+
+		public string TagsBranch { get; set; }
+
+		public string Source { get; set; }
+
+		public ForwardChangesCheck()
+		{
+			if (string.IsNullOrWhiteSpace(TagsBranch))
+				TagsBranch = "tags";
+		}
 
 		public override bool Execute()
 		{
 			var originalChangesets = OriginalChangeset.Split(new[] { ' ', ';', ',', ':' }, StringSplitOptions.RemoveEmptyEntries);
+
+			if (!string.IsNullOrWhiteSpace(Source))
+			{
+				if (!string.IsNullOrWhiteSpace(TagsBranch))
+					RunHg(string.Format("pull -r {0} {1}", TagsBranch, Source));
+
+				RunHg(string.Format("pull -r {0} {1}",
+				                    string.Join(" ", originalChangesets.Select(oc => string.Format("-r {0}", oc))),
+				                    Source));
+			}
+
 			var output = RunHg(string.Format("log --rev \"({0}) and !ancestors('{1}')\"", string.Join(" or ", originalChangesets.Select(c => string.Format("ancestors('{0}')", c))), NewChangeset));
 			
 			if (!string.IsNullOrWhiteSpace(output))
