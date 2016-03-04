ba155eeff29fa270e7b9fcb7f9bc03c6c7551be5
carl.staub <carl.staub@localhost>
2016-03-03 22:30:24 -0800

    Adding Deployment Tool.

---
+++ Code/DeploymentConfiguration/AppServer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace DeploymentTool
+{
+	public abstract class AppServer : Server
+	{
+		[XmlAttribute]
+		public string AppLocation { get; set; }
+
+		[XmlAttribute]
+		public string BackupLocation { get; set; }
+	}
+}
