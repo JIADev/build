37c57a4fb32e2e6fcff4a5fa4737d8f114504b25
carl.staub <carl.staub@localhost>
2016-06-20 17:24:27 -0700

    Fixing trash command.

--- trash.ps1
+++ trash.ps1
@@ -2,4 +2,4 @@ param ( [string]$BranchName )
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $msbuild = "c:\windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 
-& $msbuild /t:Trash /p:SourceBranch=$BranchName /p:TrashBranch=trashcan $scriptPath\buildtools.proj
+& $msbuild /t:Trash /p:SourceBranch=$BranchName /p:TrashBranch=trashcan /p:TrashRepo=c:\dev\repos\trashActiveRepo $scriptPath\buildtools.proj
