8166536e58969ad2ffe56e0c7c926c7efa7b1354
carl.staub <carl.staub@localhost>
2016-06-22 11:30:11 -0700

    Adding build for 1002_WebPWS

--- trashbranches.ps1
+++ trashbranches.ps1
@@ -1,5 +1,7 @@
-param ( [string]$BranchNames )
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+function closeBranches([string]$BranchNames) {
+$scriptPath = "c:\build"
 $msbuild = "c:\windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 
-& $msbuild /t:TrashBranches /p:SourceBranch=$BranchNames /p:TrashBranch=trashcan /p:TrashRepo=c:\dev\repos\trashActiveRepo $scriptPath\buildtools.proj
+& $msbuild /t:TrashBranches /p:SourceBranch="$BranchNames" /p:TrashBranch=trashcan /p:TrashRepo=c:\dev\repos\trashActiveRepo $scriptPath\buildtools.proj
+}
