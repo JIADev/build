992c69a5c640aa3301b9ed743031bad69789734f
Carl Staub <carl.staub@jenkon.com>
2016-10-20 19:56:39 -0700

    Enhancing merges.

---
+++ Code/SortPackageConfigs.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace j6.BuildTools.MsBuildTasks
+{
+    public class SortPackageConfigs : Task
+    {
+        [Required]
+        public string Root { get; set; }
+
+        public override bool Execute()
+        {
+            var root = new DirectoryInfo(Root);
+            var packageConfigs = root.GetFiles("packages.config", SearchOption.AllDirectories);
+            
+        
+            foreach (var child in packageConfigs.Select(cc => new { FileInfo = cc, Xml = XDocument.Load(cc.FullName) }))
+            {
+                var filePackageNodes = new List<XElement>();
+                if (child == null || child.Xml == null || child.Xml.Root == null ||
+                    !child.Xml.Root.Name.LocalName.Equals("packages", StringComparison.InvariantCultureIgnoreCase))
+                    continue;
+
+                filePackageNodes.AddRange(child.Xml.Root.Elements().OrderBy(c => c.Attribute("id").Value).ThenBy(c => Version.Parse(c.Attribute("version").Value)));
+                child.Xml.Root.RemoveAll();
+                foreach (var packageNode in filePackageNodes)
+                {
+                    child.Xml.Root.Add(packageNode);
+                }
+                child.Xml.Save(child.FileInfo.FullName);
+            }
+
+            return true;
+        }
+    }
+}
