cdf09f2917010e289968bb661e77c39a5c5fb190
Carl Staub <carl.staub@jenkon.com>
2016-11-08 14:25:09 -0800

    Removing debugging code

--- Code/SortPackageConfigs.cs
+++ Code/SortPackageConfigs.cs
@@ -16,16 +16,27 @@ namespace j6.BuildTools.MsBuildTasks
         [Required]
         public string Root { get; set; }
 
+        public bool OnlyUseLatest { get; set; }
+
         public override bool Execute()
         {
             var root = new DirectoryInfo(Root);
             var packageConfigs = root.GetFiles("packages.config", SearchOption.AllDirectories);
 
-	        var packages = packageConfigs.Select(Packages.Load);
-			
-			foreach (var package in packages.Where(p => p.Items != null))
+            var packages = packageConfigs.Select(Packages.Load).Where(p => p.Items != null).ToArray();
+            var latestVersions = new Dictionary<string, Package>();
+            if (OnlyUseLatest)
+            {
+                latestVersions = GetLatestVersions(packages.SelectMany(p => p.Items));
+            }
+			foreach (var package in packages)
 			{
-				package.SortDistinct();
+			    if (OnlyUseLatest)
+			    {
+			        var packageNames = package.Items.Select(p => p.Id).Distinct(StringComparer.InvariantCultureIgnoreCase);
+			        package.Items = packageNames.Select(p => latestVersions[p]).ToArray();
+			    }
+			    package.SortDistinct();
 				package.Save(new XmlSerializerNamespaces(new[]
 				{
 					new XmlQualifiedName(string.Empty, string.Empty), 
@@ -33,6 +44,16 @@ namespace j6.BuildTools.MsBuildTasks
 			}
 
 			return true;
+        }
+
+        private Dictionary<string, Package> GetLatestVersions(IEnumerable<Package> packages)
+        {
+            var vc = new VersionComparer();
+	        
+            var returnValue = packages.GroupBy(p => p.Id, StringComparer.InvariantCultureIgnoreCase)
+                             .ToDictionary(p => p.Key, p => p.OrderByDescending(pk => pk.VersionString, vc)
+                                                                .ThenByDescending(pk => pk.TargetFramework, StringComparer.InvariantCultureIgnoreCase).First());
+            return returnValue;
         }
     }
 }
