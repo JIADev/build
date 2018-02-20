afb27be98109ff4136f56153a1de8a2c26d0c62a
Justin Collins <Justin.Collins@jenkon.com>
2018-02-20 16:13:40 -0600

    Added ps1 to clean up before deployment. Need to wrie script to call ps1 on remote machines.

--- jenkins/deploy/jenkinsDeployCleanUp.ps1
+++ jenkins/deploy/jenkinsDeployCleanUp.ps1
@@ -21,13 +21,48 @@ param
 	[string]$sitePkgDir
 )
 
+$sitePkgLimit = (Get-Date).AddMonths(-2) #months
+$siteBaksLimit = (Get-Date).AddMonths(-6) #months
+$rtePkgLimit = (Get-Date).AddMonths(-2) #months
+$rteBaksLimit = (Get-Date).AddMonths(-6) #months
+
+$json = Get-Content $config_json -Raw | ConvertFrom-Json
+$webservers = $json.$driver.environments.$deploy_env.webservers | Get-Member -MemberType NoteProperty | select -ExpandProperty Name
+$rteServer = $json.$driver.environments.$deploy_env.rte.hostname
+$appDrive = $json.$driver.environments.$deploy_env.appDrive
+$rteDrive = $json.$driver.environments.$deploy_env.rte.rteDrive
+$appDeployPkgDir = $json.$driver.environments.$deploy_env.pkgDir
+$appBaksPkgDir = $json.$driver.environments.$deploy_env.siteBAKSDir
+$rteDeployPkgDir = $json.$driver.environments.$deploy_env.siteBAKSDir
+$rteBaksPkgDir = $json.$driver.environments.$deploy_env.siteBAKSDir
+
+$rteTempDir = "RTEstage"
+$siteReleasePkgPath = gci -Path "$sitePkgDir\LatestSite" -Recurse | where { $_ -like '*_Site.zip' } | select -ExpandProperty FullName
+
 #Clean up PKGS on web
 
-#clean up >6 month BAKS on web
+#delete PKGS older than 2 months
+$webCleanUpPath = "$($appDrive):\$appDeployPkgDir"
+gci -Path $webCleanUpPath -Recurse -File *.zip | ? { !$_.PSIsContainer -and $_.CreationTime -lt $sitePkgLimit } | Remove-Item -Force -Recurse
+
+#delete BAKS older than 6 months
+$webCleanUpPath = "$($appDrive):\$appBaksPkgDir"
+gci -Path $webCleanUpPath -Recurse -File *.zip | ? { !$_.PSIsContainer -and $_.CreationTime -lt $siteBaksLimit } | Remove-Item -Force -Recurse
+
 
 #Clean up PKGS on RTE
 
-#clean up >6 month BAKS on web
+#delete PKGS older than 2 months
+$rteCleanUpPath = "$($rteDrive):\$rteDeployPkgDir"
+gci -Path $rteCleanUpPath -Recurse -File *.zip | ? { !$_.PSIsContainer -and $_.CreationTime -lt $rtePkgLimit } | Remove-Item -Force -Recurse
 
-#Clean up "temp" on RTE
+#delete BAKS older than 6 months
+$rteCleanUpPath = "$($rteDrive):\$rteBaksPkgDir"
+gci -Path $rteCleanUpPath -Recurse -File *.zip | ? { !$_.PSIsContainer -and $_.CreationTime -lt $rteBaksLimit } | Remove-Item -Force -Recurse
 
+#Clean up "temp" on RTE if it exists
+
+if (Test-Path -Path "C:\$rteTempDir")
+{
+	gci -Path "C:\" $rteTempDir | Remove-Item -Force -Recurse
+}
