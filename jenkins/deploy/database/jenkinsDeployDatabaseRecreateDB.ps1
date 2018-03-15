541c9815c05f6b786ee6635efc20b83864984448
Justin Collins <Justin.Collins@jenkon.com>
2018-03-15 10:55:17 -0500

    adding finished recreateDB script for Jenkins, to test.

--- jenkins/deploy/database/jenkinsDeployDatabaseRecreateDB.ps1
+++ jenkins/deploy/database/jenkinsDeployDatabaseRecreateDB.ps1
@@ -18,28 +18,17 @@ param
 	[string]$deploy_env
 )
 
-$ErrorActionPreference = 'Stop'
-
 #get json config info for sql server and dbname and possibly backup drive.
 $json = Get-Content $config_json -Raw | ConvertFrom-Json
 $dbname = $json.$driver.environments.$deploy_env.sql.dbName
-$sqlHostname = $json.$driver.environments.$deploy_env.sql.hostname
-$sqlIP = $json.$driver.environments.$deploy_env.sql.ip
-$sqlBackupDir = $json.$driver.environments.$deploy_env.sql.backups
-$backupDrive = ((Split-Path -path $sqlBackupDir -Qualifier)) -replace ':',''
-$backupPath = $sqlBackupDir -replace ':', '$'
-$backupPath = "\\$sqlHostname\$backupPath"
 
-#check if DB exists, if so, drop it.
-Write-Host "If DB exists, we want to drop it."
-$dropScript = "IF EXISTS (SELECT name FROM sys.databases WHERE name = '$($dbname)') DROP DATABASE [$($dbname)]"
-#execute SQLCMD
-Write-Host "If DB doesn't exists, create the blank DB"
-$createScript = "IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = '$($dbname)') CREATE DATABASE [$($dbname)]"
-#Execute SQLCMD
-Write-Host "Once the DB is created, we need to make sure we set up permissions, and service broker is enabled."
+[System.Reflection.Assembly]::LoadFrom("$workingDirectory\Bootstrap\jDeployPowerShellTasks.dll")
+$recreatDBEngine = New-Object -TypeName jDeployPowerShellTasks.RecreateDB.PSRecreateDB
 
-$cust = $driver.Substring(4,4)
-Write-Host $workerProcessUser
-$userScript = "USE [$($dbName)] OG; DECLARE @brokerSql VARCHAR(200) GO; SET @brokerSql = 'ALTER DATABASE [' + DB_NAME() + '] SET NEW_BROKER WITH ROLLBACK IMMEDIATE' GO; EXEC (@brokerSql) GO; EXEC sp_configure 'clr enabled', 1 go; RECONFIGURE go; EXEC sp_configure 'clr enabled' go; USE [$($dbName)] GO; exec sp_adduser @loginame = 'jenkon\$($deploy_env)$($cust)_rte' GO; exec sp_addrolemember @rolename = 'db_owner', @membername = 'jenkon\$($deploy_env)$($cust)_rte' GO; exec sp_adduser @loginame = 'jenkon\$($deploy_env)$($cust)_wp' GO; exec sp_addrolemember @rolename = 'db_owner', @membername = 'jenkon\$($deploy_env)$($cust)_wp' GO; "
-#Exceute SQLCMD
+
+$request = New-Object -TypeName jDeployPowerShellTasks.RecreateDB.RecreateDBRequest
+$request.WorkingFolder = "$workingDirectory"
+$request.DatabaseName = $dbname
+
+$result = $recreatDBEngine.Execute($request)
+
