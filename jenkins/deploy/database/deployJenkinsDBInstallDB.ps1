6c41f8dbb884531bd9cf483cc814a4bfe445356b
Justin Collins <Justin.Collins@jenkon.com>
2018-03-23 10:20:25 -0500

    Added better error handling to the install DB script

--- jenkins/deploy/database/deployJenkinsDBInstallDB.ps1
+++ jenkins/deploy/database/deployJenkinsDBInstallDB.ps1
@@ -30,45 +30,49 @@ $schemaUpdateScript = "SchemaUpdate.$dbName.$($build_time).sql"
 $scriptDacPacsScriptPath = "$($ENV:ps_scripts_dir)\deploy\database\deployJenkinsNewDBScriptDacPacs.ps1"
 
 #Import the j6DeployMsBuildTask.dll to use for PatchLoader.
+Write-Host "Import the j6DeployMsBuildTask.dll to use for PatchLoader."
+
 [System.Reflection.Assembly]::LoadFrom("$workingDirectory\Bootstrap\jDeployPowerShellTasks.dll")
 
-#Make sure PatchLoader is present and assign to process variable.
-
-
 $additionalPatchDirs = "$schemaUpdateDir"
-
-
-
 
 $PSPatchEngine = New-Object -TypeName jDeployPowerShellTasks.DeployPatches.PSDeployPatches
 $PatchRequest = New-Object -TypeName jDeployPowerShellTasks.DeployPatches.DeployPatchesRequest
 $PatchRequest.WorkingFolder = "$workingDirectory"
 
 #Run PreSchema Phase
+Write-Host "Run PreSchema Phase"
 try
 {
 	$PatchRequest.Phases = @("PreSchemaUpdate")
 	$result = $PSPatchEngine.Execute($PatchRequest)
 }
-catch { Write-Error "Preschema FAILED." }
+catch
+{
+	$ErrorMessage = $_.Exception.Message
+	Write-Error $ErrorMessage
+}
 
 
 #Regenerate SchemaUpdate scripts
+Write-Host "Regenerate SchemaUpdate scripts"
 
 $scriptArgs = "$driver $config_json $deploy_env $build_time $workingDirectory"
-Invoke-Command -ComputerName "JIA-JENKINS1" { "$($ENV:ps_scripts_dir)\deploy\database\deployJenkinsNewDBScriptDacPacs.ps1" } -ArgumentList $driver,$config_json,$deploy_env,$build_time,$workingDirectory
-
-	#Invoke-Command "$($ENV:ps_scripts_dir)\deploy\database\deployJenkinsNewDBScriptDacPacs.ps1 -driver $driver -config_json $config_json -deploy_env $deploy_env -build_time $build_time -workingDirectory $workingDirectory"
-#	& $scriptDacPacsScriptPath $scriptArgs
+Invoke-Command -ComputerName "JIA-JENKINS1" { "$($ENV:ps_scripts_dir)\deploy\database\deployJenkinsNewDBScriptDacPacs.ps1" } -ArgumentList $driver, $config_json, $deploy_env, $build_time, $workingDirectory
 
 #Run SchemaUpdate and DataPatch (PostSchema) phases
+Write-Host "Run SchemaUpdate and DataPatch (PostSchema) phases"
 try
 {
 	
 	$PatchRequest.Phases = @("SchemaUpdate", "DataPatch")
 	$result = $PSPatchEngine.Execute($PatchRequest)
 }
-catch {Write-Error "SchemaUpdate FAILED"}
+catch
+{
+	$ErrorMessage = $_.Exception.Message
+	Write-Error $ErrorMessage
+}
 
 
 
