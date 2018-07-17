2c03d2b3f9ee32895d49593a482d6bcb7e62d8d6
RCJenkon <rcarruthers@jenkon.com>
2018-07-17 10:31:27 -0500

    flush

---
+++ jcmd/Flush.ps1
@@ -0,0 +1,63 @@
+<#
+.SYNOPSIS
+  Flushes j6 related services that are associated with cached data.
+.DESCRIPTION
+  Flushes j6 related services that are associated with cached data.
+
+.PARAMETER services
+  A string array of services from the following list that should be 
+  restarted or flushed.
+
+  Options:
+  IIS, Redis, SQL, IISExpress, All
+
+  Example: jcmd flush IIS,Redis,SQL
+
+.EXAMPLE
+  PS C:\dev\project_folder>jcmd flush IIS,Redis,SQL
+
+  .EXAMPLE
+  PS C:\dev\project_folder>jcmd flush all
+.NOTES
+  Created by Richard Carruthers 07/17/2018
+#>
+param(
+    [Parameter(Mandatory=$true,Position=0)][String[]] $services
+)
+
+. "$PSScriptRoot\_Shared\common.ps1"
+
+$all = $services -contains "all"
+$iis = $services -contains "iis"
+$sql = $services -contains "sql"
+$redis = $services -contains "redis"
+$iisexpress = $services -contains "iisexpress"
+
+Write-ColorOutput "Starting jobs:" Cyan
+
+if ($all -or $iisexpress)
+{
+    Start-Job -name FlushIISExpress -ScriptBlock {get-process | where { $_.ProcessName -like "IISExpress"} | stop-process}
+}
+
+if ($all -or $sql)
+{
+    Start-Job -name FlushSQL -ScriptBlock {Restart-Service MSSQLSERVER -force}
+}
+
+if ($all -or $iis)
+{
+    Start-Job -name FlushIIS -ScriptBlock {IISReset}
+}
+
+if ($all -or $redis)
+{
+    Start-Job -name FlushRedis -ScriptBlock {C:\Redis\redis-cli.exe $args[0]} -ArgumentList @("flushall")
+}
+
+Write-ColorOutput "Waiting for jobs to complete..." Cyan
+
+Get-Job -name Flush* | Wait-Job
+Remove-Job -name Flush*
+
+Write-ColorOutput "Flush complete..." Cyan
