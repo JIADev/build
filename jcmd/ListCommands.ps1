e38341b1f29a028f2ae4c3b11b3b4736d2c240b1
RCJenkon <rcarruthers@jenkon.com>
2018-07-25 16:47:04 -0500

    jcmd better list commands logic

--- jcmd/ListCommands.ps1
+++ jcmd/ListCommands.ps1
@@ -5,8 +5,23 @@
   Lists jcmd commands with synopsis.
 .EXAMPLE
   PS C:\dev\project_folder>jcmd listcommands
+.PARAMETER quiet
+  Limits output to only a list of commands
+.PARAMETER fullName
+  Limits output to only a list of commands
 .NOTES
 #>
+param(
+  [switch]$quiet,
+  [switch]$fullName
+)
+
+function GetCommandName($fileName, $showFullName)
+{
+  if ($showFullName) {return $fileName}
+  $shortName = [IO.Path]::GetFileNameWithoutExtension($fileName);
+  return $shortName;
+}
 
 . "$PSScriptRoot\_Shared\common.ps1"
 
@@ -20,12 +35,21 @@ $folderCommands =
     ForEach-Object {$leaf=Split-Path -Leaf $_; return "$_\$leaf.ps1"} |
     Where-Object {Test-Path $_}
 
-($commands + $folderCommands) |
-    ForEach-Object { Get-Help $_ } | 
-    Select-Object @{N="Command Name"; E={[IO.Path]::GetFileNameWithoutExtension($_.Name)}},@{N="Description"; E={$_.Synopsis}} | 
-    Sort-Object -Property "Command Name" | 
+if ($quiet)
+{
+  ($commands + $folderCommands) |
+    ForEach-Object {$(GetCommandName $_ $fullName)}    
+}
+else {
+  ($commands + $folderCommands) |
+    ForEach-Object { Get-Help $_ } |
+    Select-Object @{N="Command Name"; E={GetCommandName($_.Name,$fullName)}},@{N="Description"; E={$_.Synopsis}} |
+    Sort-Object -Property "Command Name" |
     Format-Table -Auto
 
-Write-ColorOutput "Execute: jcmd help [Command] for details on a specific command" -ForegroundColor Cyan
+    Write-ColorOutput "Execute: jcmd help [Command] for details on a specific command" -ForegroundColor Cyan
+}
+
+
 
 
