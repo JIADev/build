a8676c7288a8a1bfb9cf21730df79507bf7174da
RCJenkon <rcarruthers@jenkon.com>
2018-07-26 14:22:45 -0500

    jcmd UpdateJcmd comments

--- jcmd/UpdateJcmd.ps1
+++ jcmd/UpdateJcmd.ps1
@@ -14,22 +14,28 @@
 
 $cmdFolder = "$PSScriptRoot\..";
 
+#switch to the jcmd folder
 Push-Location $cmdFolder
+#get the resolved folder name
 $folder = Get-Location
 try {
   Write-ColorOutput "Updating $folder from source control..." -ForegroundColor Cyan
+  
+  #make sure there are no pending changes
   $hasPendingChanges = SourceControl_HasPendingChanges
   if ($hasPendingChanges -eq $true) {
     Write-ColorOutput "Cannot update " Red
     Exit 1
   }
 
+  #get the current branch and update
   $branch = SourceControl_GetCurrentBranch
   SourceControl_UpdateBranchToHead $branch
 
   Write-ColorOutput "Update Complete" -ForegroundColor Cyan
 }
 finally {
-  Pop-Location
+	#switch back to the user's path
+	Pop-Location
 }
 
